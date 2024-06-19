126113b90c43f5a5c0222d938eb40dfe32b86a6e
RONALD FABRIZIO HURTADO RAMOS <maxx1212_2@hotmail.com>
2024-06-19 11:33:30 -0500

    Fix error with NotaID

--- src/main/java/com/example/sem8/repositorio/NotaRepository.java
+++ src/main/java/com/example/sem8/repositorio/NotaRepository.java
@@ -1,6 +1,7 @@
 package com.example.sem8.repositorio;
 
+import com.example.sem8.entidad.NotaID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.sem8.entidad.Nota;
-public interface NotaRepository extends JpaRepository<Nota, Long> {
+public interface NotaRepository extends JpaRepository<Nota, NotaID> {
 }
